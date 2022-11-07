77acbcdca5269e21bec5e69cde678b77075ac1eb
maxbtchr <maxboucher.son@gmail.com>
2022-11-07 09:28:44 -0500

    project completed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,116 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- eslint-disable prettier/prettier -->
+  <div>
+
+    <ScoreBoard :winCount="this.answerCount.winCount" :loseCount="this.answerCount.loseCount"/>
+
+    <template v-if="this.question">
+
+      <h1 v-html="this.question"></h1>
+
+      <template v-for="(answer, index) in this.answers" :key="index">
+        <input
+          :disabled="this.answerSubmitted" 
+          type="radio" 
+          name="options" 
+          :value="answer"
+          v-model="this.chosenAnswer"
+        />
+  
+          <label v-html="answer"></label><br />
+      </template>
+
+      <button
+        v-if="!this.answerSubmitted"
+        @click="this.submitAnswer()" 
+        class="send" 
+        type="button"
+      >Send
+      </button>
+
+      <section v-if="this.answerSubmitted" class="result">
+        <h4 v-if="this.chosenAnswer == this.correctAnswer" 
+          v-html="'&#9989; Congratulations, the answer &quot' + this.chosenAnswer + '&quot is correct.'">
+          
+        </h4>
+        <h4 v-else
+          v-html="'&#10060; Sorry, you picked the wrong answer. The correct answer is &quot' + this.correctAnswer + '&quot.'">
+        </h4>
+        <button @click="this.getNewQuestion()" class="send" type="button">Next question</button>
+      </section>
+
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ScoreBoard from "./components/ScoreBoard.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ScoreBoard,
+  },
+
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswer: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      answerCount: {
+        winCount: 0,
+        loseCount: 0,
+      },
+    };
+  },
+  computed: {
+    answers() {
+      var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+      answers.splice(
+        Math.round(Math.random() * answers.length),
+        0,
+        this.correctAnswer
+      );
+      return answers;
+    },
+  },
+  methods: {
+    submitAnswer() {
+      if (!this.chosenAnswer) {
+        alert("Pick one of the options");
+      } else {
+        this.answerSubmitted = true;
+        if (this.chosenAnswer == this.correctAnswer) {
+          this.answerCount.winCount++;
+        } else {
+          this.answerCount.loseCount++;
+        }
+      }
+    },
+    getNewQuestion() {
+      window.localStorage.setItem(
+        "answerCount",
+        JSON.stringify(this.answerCount)
+      );
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+      this.axios
+        .get("https://opentdb.com/api.php?amount=1&category=18")
+        .then((response) => {
+          this.question = response.data.results[0].question;
+          this.incorrectAnswers = response.data.results[0].incorrect_answers;
+          this.correctAnswer = response.data.results[0].correct_answer;
+        });
+    },
+  },
+  created() {
+    this.answerCount = localStorage.getItem("answerCount")
+      ? JSON.parse(localStorage.getItem("answerCount"))
+      : this.answerCount;
+    this.getNewQuestion();
   },
 };
 </script>
@@ -21,6 +122,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px auto;
+  max-width: 960px;
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+    <div>
+        <section class="score"> Player <span>{{this.winCount}}</span> x <span>{{this.loseCount}}</span> Computer </section>
+    </div>
+</template>
+<!-- eslint-disable prettier/prettier -->
+
+<script>
+export default {
+    props: ['winCount', 'loseCount']
+};
+</script>
+<!-- eslint-disable prettier/prettier -->
+<style lang="scss">
+section.score {
+    border-bottom: 1px solid black;
+    padding: 24px;
+    font-size: 18px;
+    span {
+        padding: 8px;
+        font-weight: bold;
+        border: 1px solid black;
+    }
+}
+
+
+</style>
